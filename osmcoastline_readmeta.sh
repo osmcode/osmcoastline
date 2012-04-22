884fa8788bc2f352efad46e7f18fd62f4eb01b9d
Jochen Topf <jochen@topf.org>
2012-04-22 10:33:25 +0200

    Extend readmeta script

--- osmcoastline_readmeta.sh
+++ osmcoastline_readmeta.sh
@@ -26,6 +26,9 @@ sqlite3 $DBFILE "SELECT CASE split_large_polygons WHEN 0 THEN 'no' ELSE 'yes' EN
 echo -n "  Create water polygons (--water/-w): "
 sqlite3 $DBFILE "SELECT CASE water WHEN 0 THEN 'no' ELSE 'yes' END FROM options;"
 
+echo -n "  Spatial reference system (--srid/-s): "
+sqlite3 $DBFILE "SELECT CASE srid WHEN 4326 THEN '4326 (WGS84)' WHEN 3857 THEN '3857 (Mercator)' ELSE srid END FROM geometry_columns WHERE f_table_name='polygons';"
+
 echo "\nMetadata:\n"
 
 echo -n "  Database created at: "
@@ -42,4 +45,10 @@ sqlite3 $DBFILE "SELECT num_land_polygons_before_split FROM meta;"
 
 echo -n "  Number of land polygons after split (0 if not split): "
 sqlite3 $DBFILE "SELECT num_land_polygons_after_split FROM meta;"
+
+echo "\nErrors/warnings on points:\n"
+echo ".width 3 20\nSELECT count(*), error FROM error_points GROUP BY error;" | sqlite3 -column $DBFILE | sed -e 's/^/  /'
+
+echo "\nErrors/warnings on linestrings:\n"
+echo ".width 3 20\nSELECT count(*), error FROM error_lines GROUP BY error;" | sqlite3 -column $DBFILE | sed -e 's/^/  /'
 
