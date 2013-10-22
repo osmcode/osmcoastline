baa2b07251dd9c3add9727fde427dad5af95dfb8
Jochen Topf <jochen@topf.org>
2013-10-22 22:17:39 +0200

    Use exec in runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #valgrind --leak-check=full --show-reachable=yes
-./osmcoastline --debug --verbose --overwrite --output-lines --output-polygons=both --output-rings -o testdata.db testdata.osm
+exec ./osmcoastline --debug --verbose --overwrite --output-lines --output-polygons=both --output-rings -o testdata.db testdata.osm
 
