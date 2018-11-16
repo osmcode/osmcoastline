7b4e7a3cd09e98c5b7ff404565ed8926af38d8d8
Jochen Topf <jochen@topf.org>
2018-11-16 09:22:27 +0100

    Give tests better names.

--- test/init.sh
+++ test/init.sh
@@ -4,12 +4,11 @@ readonly SRC_DIR=$1
 readonly BIN_DIR=$2
 readonly TEST_ID=$3
 
-readonly DATA=${SRC_DIR}/test/t/test-${TEST_ID}.osm
 readonly OSMC=${BIN_DIR}/src/osmcoastline
-readonly INPUT=${BIN_DIR}/test/test-${TEST_ID}.opl
-readonly LOG=${BIN_DIR}/test/test-${TEST_ID}.log
-readonly DB=${BIN_DIR}/test/test-${TEST_ID}.db
-readonly DUMP=${BIN_DIR}/test/test-${TEST_ID}.dump
+readonly INPUT=${BIN_DIR}/test/${TEST_ID}.opl
+readonly LOG=${BIN_DIR}/test/${TEST_ID}.log
+readonly DB=${BIN_DIR}/test/${TEST_ID}.db
+readonly DUMP=${BIN_DIR}/test/${TEST_ID}.dump
 readonly SQL="spatialite -bail -batch $DB"
 
 check_count() {
